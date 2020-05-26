c27556ea0d0bda151aa9caca3bc744cdf979a0de
basny_gg <basny_gg@DESKTOP-3JR56L9>
2020-05-26 15:45:55 -0500

    this is my latest upadate

---
+++ Homepractice/src/test/java/com/Testrunner/TestRunner.java
@@ -0,0 +1,22 @@
+package com.Testrunner;
+
+
+public class TestRunner {
+	
+	public class TestRunnerNG {
+		
+		@CucumberOptions(
+		
+				features= {"Features"} ,
+				glue = {"com.nexttech.stepdef"} 
+				
+			)
+				
+		
+		
+	//class TestRunner extends AbstractTestNGCucumberTests{
+			
+	//	}
+		
+
+	}
